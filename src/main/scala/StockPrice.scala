c11c9752acb2dc4f67cc2f9716d740606f09d3c8
Yash-Chopra20BCS6402 <99278322+Yash-Chopra20BCS6402@users.noreply.github.com>
2024-04-19 15:49:54 +0530

    Commit

--- src/main/scala/StockPrice.scala
+++ src/main/scala/StockPrice.scala
@@ -1,30 +1,28 @@
 object StockPrice extends App {
-
   def maxProfit(prices: Array[Int]): Either[String, Int] = {
-    if (prices.isEmpty) Left("Input array is empty")
-    else {
-      var minPrice = prices(0)
-      var maxProfit = 0
-
-      for (i <- 1 until prices.length) {
-        maxProfit = math.max(maxProfit, prices(i) - minPrice)
-        minPrice = math.min(minPrice, prices(i))
+    def maxProfitHelper(prices: Array[Int], minPrice: Int, maxProfit: Int): Int = {
+      if (prices.isEmpty) maxProfit
+      else {
+        val newMinPrice = math.min(minPrice, prices.head)
+        val newMaxProfit = math.max(maxProfit, prices.head - minPrice)
+        maxProfitHelper(prices.tail, newMinPrice, newMaxProfit)
       }
-
-      Right(maxProfit)
     }
+
+    if (prices.isEmpty) Left("Input array is empty")
+    else Right(maxProfitHelper(prices.tail, prices.head, 0))
   }
 
-  // Test Cases for checking both the constraints
+  // Test cases
   val stockPrices1 = Array(7, 1, 5, 3, 6, 4)
   maxProfit(stockPrices1) match {
     case Left(error) => println("Error: " + error)
-    case Right(profit) => println("Maximum Profit (Case 1): " + profit) // case 1: Output should be 5
+    case Right(profit) => println("Maximum Profit (Case 1): " + profit) // Output should be 5
   }
 
   val stockPrices2 = Array.empty[Int]
   maxProfit(stockPrices2) match {
-    case Left(error) => println("(Case 2): " + error) // cae 2: Output should be "Input array is empty"
+    case Left(error) => println("Error: " + error) // Output should be "Input array is empty"
     case Right(profit) => println("Maximum Profit (Case 2): " + profit)
   }
 }
